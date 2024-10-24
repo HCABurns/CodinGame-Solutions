e99a6f8959c31ee3269f10c19869ca3c69fc3b98
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-10-24 18:43:40 +0100

    Update Python.py

--- Code Golf/Medium/Network Cabling/Python.py
+++ Code Golf/Medium/Network Cabling/Python.py
@@ -1,4 +1,3 @@
-n=int(input());s=[];u=[]
-for _ in"1"*n:x,y=map(int,input().split());s+=[x];u+=[y]
-u.sort()
-print(sum(abs(u[n//2]-v)for v in u)+max(s)-min(s))
+I=input
+n=int(I());s,u=zip(*(map(int,I().split())for _ in[0]*n))
+u=sorted(u);I(sum(abs(u[n//2]-v)for v in u)+max(s)-min(s))
