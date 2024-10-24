43f2bb52e082cc5a0e088c65091b6e3c9f1605fd
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-10-24 14:46:50 +0100

    Create Python.py

---
+++ Puzzles/Medium/Telephone Numbers/Python.py
@@ -0,0 +1,12 @@
+# Create set for storing numbers.
+o=set()
+size = int(input())
+
+# For each numbers, add every substring starting from the first position to the array. 
+for _ in range(size):
+    number = input()
+    for j in range(len(number)):
+        o.add(number[0:j+1])
+
+# Output the length of the set
+print(len(o))
