93c2f241ca8c8c5e5a46ba9cb0ee1fbc926e04ad
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-10-24 14:47:05 +0100

    Update Python.py

--- Puzzles/Medium/Telephone Numbers/Python.py
+++ Puzzles/Medium/Telephone Numbers/Python.py
@@ -2,7 +2,7 @@
 o=set()
 size = int(input())
 
-# For each numbers, add every substring starting from the first position to the array. 
+# For each numbers, add every substring starting from the first position to the set. 
 for _ in range(size):
     number = input()
     for j in range(len(number)):
