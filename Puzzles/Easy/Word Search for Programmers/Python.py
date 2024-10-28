e0a7edc4ab3ac6faf855d72f5f6ffe28fc7a6315
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-10-28 11:48:22 +0000

    Update Python.py

--- Puzzles/Easy/Word Search for Programmers/Python.py
+++ Puzzles/Easy/Word Search for Programmers/Python.py
@@ -21,31 +21,33 @@ for i in range(len(s)):
         # Convert position in string to grid
         new_i = i//n
         new_j = i%n
-        #Forwards and backwards
+        #Forwards and backwards (Example of slightly better readability)
         if s[i:i+size] in clues or s[i:i+size][::-1] in clues:
-            clues.remove(s[i:i+size] if s[i:i+size] in clues else s[i:i+size][::-1])
-            grid[i//n][i%n:i%n+size] = s[i:i+size]
+            word = s[i:i+size]
+            reversed_word = s[i:i+size][::-1]
+            clues.remove(word if word in clues else reversed_word)
+            grid[new_i][i%n:i%n+size] = word
         
         #Up and Down
         if s[i:i+(n*size):n] in clues or s[i:i+(n*size):n][::-1] in clues:
             clues.remove(s[i:i+(n*size):n] if s[i:i+(n*size):n] in clues else s[i:i+(n*size):n][::-1] )
-            for k , letter in zip(range(size), s[i:i+(n*size):n]) :
-                grid[new_i%n][new_j%n] = letter
+            for char in s[i:i+(n*size):n]:
+                grid[new_i%n][new_j] = char
                 new_i += 1
         
         #Diagonal Down
         if s[i:i+(n*size):n+1] in clues or s[i:i+(n*size):n+1][::-1] in clues:
             clues.remove(s[i:i+(n*size):n+1] if s[i:i+(n*size):n+1] in clues else s[i:i+(n*size):n+1][::-1])
-            for letter in s[i:i+(n*size):n+1]:
-                grid[new_i%n][new_j%n] = letter
+            for char in s[i:i+(n*size):n+1]:
+                grid[new_i%n][new_j%n] = char
                 new_i += 1
                 new_j += 1
         
         #Diagonal Up
         if s[i:i+((n-1)*size):n-1] in clues or s[i:i+((n-1)*size):n-1][::-1] in clues:
             clues.remove(s[i:i+((n-1)*size):n-1] if s[i:i+((n-1)*size):n-1] in clues else s[i:i+((n-1)*size):n-1][::-1])
-            for letter in s[i:i+((n-1)*size):n-1]:
-                grid[new_i%n][new_j%n] = letter
+            for char in s[i:i+((n-1)*size):n-1]:
+                grid[new_i%n][new_j%n] = char
                 new_i += 1
                 new_j -= 1
 
