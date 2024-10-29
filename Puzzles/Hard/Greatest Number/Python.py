2671f8c3f45369ee234ebb0d099d54f1f5767eae
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-10-29 19:23:06 +0000

    Update Python.py

--- Puzzles/Hard/Greatest Number/Python.py
+++ Puzzles/Hard/Greatest Number/Python.py
@@ -2,14 +2,6 @@
 n = int(input())
 s = input().split(" ")
 
-# Check if the number is positive or negative. Also check if the number is a float or integer.
-negative = False
-decimal = False
-if "-" in s:
-    negative = True
-if "." in s:
-    decimal = True
-
 #Sort numbers only smallest to largest.
 vals = sorted([i for i in s if i.isnumeric()])
 
@@ -19,11 +11,11 @@ if all(1 if i == "0" else 0 for i in vals):
     quit()
 
 # If deciaml add deciaml.
-if decimal:
+if "." in s:
     vals.insert(1,".")
 
 # If negative add minus sign otherwise reverse the array.
-if negative:
+if "-" in s:
     vals = ["-"] + vals
 else:
     vals = vals[::-1]
