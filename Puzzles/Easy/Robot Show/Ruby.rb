ce27f9e9a7297556bb506b9587b44ba64fd1a0aa
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-10-31 13:45:32 +0000

    Update Ruby.rb

--- Puzzles/Easy/Robot Show/Ruby.rb
+++ Puzzles/Easy/Robot Show/Ruby.rb
@@ -4,8 +4,8 @@ n = gets.to_i
 
 # Create array of bot positions.
 arr = []
-inputs = gets.split
-for i in 0..(n-1){arr << inputs[i].to_i}
+inputs = gets.split(" ")
+inputs.each{|i| arr << i.to_i}
 
 # Max time is for a furthest box from the end to reach the end.
 puts [l-arr.min,arr.max].max
