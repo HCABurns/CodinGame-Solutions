47b70cd81f7f25b4fda92f7012be2f064868580f
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-11-02 16:35:13 +0000

    Update Python.py

--- Puzzles/Easy/The Other Side/Python.py
+++ Puzzles/Easy/The Other Side/Python.py
@@ -8,10 +8,12 @@ grid = [list(input().split()) for _ in range(h)]
 # Function to check if there's a path from (i, j) to the rightmost column
 def can_reach_end(i, j, visited):
     # If reached the rightmost column, return True
-    if j == w - 1:
+    if j == w - 1 or grid[i][j]=="P":
         return True
     
     # Mark the cell as visited
+    original = grid[i][j]
+    grid[i][j] = "P" # Set to P for easier searching of large grids.
     visited.add((i, j))
 
     # Explore the four possible directions (down, right, up, left)
@@ -19,10 +21,14 @@ def can_reach_end(i, j, visited):
         iy, jx = i + y, j + x
 
         # Check bounds and if the cell is traversable and not visited
-        if 0 <= iy < h and 0 <= jx < w and grid[iy][jx] == '+' and (iy, jx) not in visited:
-            if can_reach_end(iy, jx, visited):
+        if 0 <= iy < h and 0 <= jx < w and (iy,jx) not in visited:
+            if grid[iy][jx] == '+':
+                if can_reach_end(iy, jx, visited):
+                    return True
+            elif grid[iy][jx] == "P":
                 return True
 
+    grid[i][j] = original # Return to original if not found.
     return False
 
 # Count the number of cells in the left column that can reach the rightmost column
@@ -31,6 +37,8 @@ for i in range(h):
     if grid[i][0] == '+':
         if can_reach_end(i, 0, set()):
             count_reachable += 1
+    elif grid[i][0] == "P":
+        count_reachable += 1
 
 # Output the count.
 print(count_reachable)
