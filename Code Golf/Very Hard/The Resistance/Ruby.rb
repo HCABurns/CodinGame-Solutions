e5eb9b1455eb4cd3b7facb5b257114799816deda
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-11-06 01:13:40 +0000

    Update Ruby.rb

--- Code Golf/Very Hard/The Resistance/Ruby.rb
+++ Code Golf/Very Hard/The Resistance/Ruby.rb
@@ -2,7 +2,5 @@ m=gets.chop
 w=[]
 gets.to_i.times{w<<gets.chomp.chars.map{|c|Hash[('A'..'Z').zip %w[.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..]][c]}.join}
 c={}
-def s(i,m,w,c)
- return 1 if i==m.size;return c[i] if c[i];t=0;w.each{|x|t+=s(i+x.size,m,w,c)if m[i,x.size]==x};c[i]=t
-end
-puts s(0,m,w,c)
+s=->(i,m,w,c){return 1if i==m.size;return c[i]if c[i];t=0;w.each{|x|t+=s[i+x.size,m,w,c]if m[i,x.size]==x};c[i]=t}
+p s[0,m,w,c]
