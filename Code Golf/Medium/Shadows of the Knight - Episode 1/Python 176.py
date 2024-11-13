9e81e838a7cb3877cf19aadbefede7efd6b649d5
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-11-13 14:30:59 +0000

    Create Python 176.py

---
+++ Code Golf/Medium/Shadows of the Knight - Episode 1/Python 176.py
@@ -0,0 +1,9 @@
+I=input
+w,h,_,x,y=map(int,(I()+" "+I()+" "+I()).split())
+X=Y=0
+while d:=I():
+ if"U"in d:h=y-1
+ if"D"in d:Y=y+1
+ if"L"in d:w=x-1
+ if"R"in d:X=x+1
+ print(x:=(X+w)//2,y:=(Y+h)//2)
