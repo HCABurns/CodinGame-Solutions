b1c897f845834f5e4e813847e964ba4173eb44e0
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-11-14 22:53:23 +0000

    Update Python.py

--- Puzzles/Easy/Retro Typewriter Art/Python.py
+++ Puzzles/Easy/Retro Typewriter Art/Python.py
@@ -3,17 +3,12 @@ special_character_hashmap = {"sp":" ", "bS":"\\", "sQ":"'", "nl":"\n"}
 numbers_set = {"1","2","3","4","5","6","7","8","9","0"}
 
 for i in input().split(" "):
-    # Check if newline character - If found print newline and continue (Move on to next item from input)
-    if i == "nl":
-        print("")
-        continue
-
     # Check if all numbers in input. If so then output.
     if all([1 if j in numbers_set else 0 for j in i]):
         print(i[-1]*int(i[:-1]),end="")
 
-    # Split the input into numbers and characters.
-    quantity = int("".join([j for j in i if j in numbers_set]))
+    # Split the input into numbers and characters. (Minimum quantity = 1)
+    quantity = 1 if (quantity:="".join([j for j in i if j in numbers_set])) == "" else int(quantity)
     string = "".join([j for j in i if j not in numbers_set])
 
     # Print correct char * quantity (Replace special characters with char in hashmap)
@@ -22,4 +17,4 @@ for i in input().split(" "):
         o=special_character_hashmap[string]*quantity
     else:
         o=string*quantity
-    print(o,end="")
+    print(end=o)
