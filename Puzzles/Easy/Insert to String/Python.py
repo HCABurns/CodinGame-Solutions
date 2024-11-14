0f346d0771ad2707dd4cf546aabbeb7d9e529abc
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-11-14 12:29:35 +0000

    Create Python.py

---
+++ Puzzles/Easy/Insert to String/Python.py
@@ -0,0 +1,26 @@
+# Get input and convert \n to new lines, and convert to list for malleable strings.
+s = [list(i) for i in input().split("\\n")]
+
+# Hashmap for checking new inserts are in correct location.
+added = {}
+
+# Get number of changes to make.
+change_count = int(input())
+for i in range(change_count):
+    # Get change and convert ints. 
+    line, idx, change = input().split("|")
+    line , idx = map(int, [line, idx])       
+
+    # Find out how many additions have been made before current id to ensure added to correct location.
+    to_change = sum([1 if val < idx else 0 for val in added.get(line,[])])
+    
+    # Insert int correct location.
+    s[line].insert(idx+to_change,change)
+
+    # Update hashamp but new additons index on the correct line.
+    if line not in added:
+        added[line] = []
+    added[line]+=[idx]
+
+# Output the new string with the inserts.
+print(*"\n".join(["".join(j) for j in s]).split('\\n'),sep="\n")
