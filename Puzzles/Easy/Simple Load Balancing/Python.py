1a407aae441a9208f16cc0261dad09093882642e
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-11-15 20:12:48 +0000

    Update Python.py
    Change name of array to more meaningful idfentifier.

--- Puzzles/Easy/Simple Load Balancing/Python.py
+++ Puzzles/Easy/Simple Load Balancing/Python.py
@@ -1,8 +1,8 @@
 # Get inputs and store loads in array. (Sort loads for better efficiency)
 n = int(input())
 k = int(input())
-start = list(map(int,input().split()))
-start.sort()
+servers = list(map(int,input().split()))
+servers.sort()
 
 # Define required variables.
 idx = 0
@@ -10,29 +10,29 @@ additional = 0
 remainder = 0
 
 # Continue until all same length or no more to add.
-while k > 0 and idx<len(start)-1:
+while k > 0 and idx<len(servers)-1:
     # Get difference and increment idx.
-    diff = start[idx+1] - start[idx]
+    diff = servers[idx+1] - servers[idx]
     idx+=1
     # If all minimums can be incremented to next unique minimum, do it.
     if k > idx * diff:
         for j in range(idx):
-            start[j] += diff
+            servers[j] += diff
             k -= diff
     else:
         # Add 1 to a item if remainder exists.
         additional = k // idx
         if additional!=0 and k % additional > 0:
-            start[0] += 1
+            servers[0] += 1
         # Increment all 
         for j in range(idx):
-            start[j] += additional
+            servers[j] += additional
         k = 0
 
 # Add remainder to first if needed.
 additional = k // idx
 if additional!=0 and k % additional > 0:
-    start[-1] += 1
+    servers[-1] += 1
 
 # Output the minimum difference.
-print(max(start) - min(start) + remainder)
+print(max(servers) - min(servers) + remainder)
