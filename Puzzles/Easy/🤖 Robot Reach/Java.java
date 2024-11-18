a9df3ab922d3005559ed5e5d4ff51395125f9e1f
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-11-18 11:53:04 +0000

    Update Java.java
    Simplicity fix.

--- Puzzles/Easy/🤖 Robot Reach/Java.java
+++ Puzzles/Easy/🤖 Robot Reach/Java.java
@@ -13,11 +13,10 @@ class Solution {
         // Check each position and ensure value is below threshold and the cell is reachable from other valid cells.
         for (int i = 0; i<HEIGHT;i++){
             for (int j = 0; j < WIDTH; j++){
-                if (i==0 || visited.contains(String.valueOf(i-1) + " " + String.valueOf(j)) ||
-                    visited.contains(String.valueOf(i) + " " + String.valueOf(j-1))){
+                if (i==0 || visited.contains((i-1)+" "+j) || visited.contains(i+" "+(j-1))){
                     int sum_val = ("" + i + j).chars().map(Character::getNumericValue).sum();
                     if (sum_val <= THRESHOLD){
-                        visited.add(String.valueOf(i) + " " + String.valueOf(j));
+                        visited.add(i+" "+j);
                     }
                 }
             }
