aa91e3ae9a8a056c113c14fcc34e7ad440e61d9f
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-12-19 13:52:21 +0000

    Delete Puzzles/Tutorial directory
    Moved to correct position in Easy puzzles.

--- Puzzles/Tutorial/Clojure.clj
+++
@@ -1,9 +0,0 @@
-(ns Player
-  (:gen-class))
-
-(defn -main [& args]
-  (while true
-    (let [enemy1 (read) dist1 (read) enemy2 (read) dist2 (read)]
-      ; Print closer enemy.
-      (if (< dist1 dist2) (println enemy1) (println enemy2))
-    )))
