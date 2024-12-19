75b23d89502d4c110f63ba66d32b74c9249927fe
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2024-12-19 00:01:34 +0000

    Create Perl.pl

---
+++ Puzzles/Tutorial/Perl.pl
@@ -0,0 +1,15 @@
+select(STDOUT); $| = 1; # DO NOT REMOVE
+
+while (1) {
+    chomp(my $enemy_1 = <STDIN>); # name of enemy 1
+    chomp(my $dist_1 = <STDIN>); # distance to enemy 1
+    chomp(my $enemy_2 = <STDIN>); # name of enemy 2
+    chomp(my $dist_2 = <STDIN>); # distance to enemy 2
+    
+    # Print closer enemy.
+    if ($dist_1 < $dist_2) {
+        print "$enemy_1\n";
+    } else {
+        print "$enemy_2\n";
+    }
+}
