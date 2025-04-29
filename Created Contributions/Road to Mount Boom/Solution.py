fe13fab27956c1bd7cc09a9c0cebc1aa8aa0b5f6
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2025-04-29 11:24:57 +0100

    Update Solution.py

--- Created Contributions/Road to Mount Boom/Solution.py
+++ Created Contributions/Road to Mount Boom/Solution.py
@@ -1,8 +1,7 @@
 # Form grid, find Brodo and Mount Boom.
-w, h = [int(i) for i in input().split()]
+h, w = [int(i) for i in input().split()]
 grid = [[] for _ in range(h)]
-brodo = boom = ()
-#boom = ()
+brodo = boom = (0,0)
 for i in range(h):
     row = input()
     for j,char in enumerate(row):
@@ -13,12 +12,12 @@ for i in range(h):
         elif char == "M":
             boom = (i,j)
 
-# Define the required variables.
+# Define the required variables to perfrom BFS.
 to_visit = [brodo]
 directions = [(-1, 0), (0, 1), (1, 0), (0, -1), (1,1),(-1,-1),(1,-1),(-1,1)]
 visited = set()
 
-# Check that the diagonal move is not going through connected mountains.
+# Store relative positions to check if a diagonal move is not going through connected mountains.
 routes = {(1,1):[(1,0),(0,1)],
 (-1,-1):[(-1,0),(0,-1)],
 (-1,1):[(-1,0),(0,1)],
@@ -27,25 +26,26 @@ routes = {(1,1):[(1,0),(0,1)],
 # Perfrom BFS, with early exit, to find distance from Frodo to Mount Boom.
 while to_visit:
     current = to_visit.pop(0)
+    i, j = current
     visited.add(current)
+    # Early Exit Clause
     if current == boom:
         break
     
     for iy, jx in directions:
-        # Check if the diagonal path is valid or blocked by mountains.
-        if ((iy,jx) in routes):
-            ti, tj = routes[(iy,jx)]
-            if "^" == grid[ti[0]+current[0]][ti[1]+current[1]] == grid[tj[0]+current[0]][tj[1]+current[1]]:
-                print("Yes: ", current , "direction",iy, jx)
-                print(grid[ti[0]+current[0]][ti[1]+current[1]], grid[tj[0]+current[0]][tj[1]+current[1]])
+        # Check if the diagonal path is valid or blocked by connected mountains.
+        if (iy,jx) in routes:
+            diag1, diag2 = routes[(iy,jx)]
+            if "^" == grid[diag1[0]+i][diag1[1]+j] == grid[diag2[0]+i][diag2[1]+j]:
                 continue
 
-        iy = current[0] + iy
-        jx = current[1] + jx
+        # If move has not been visited, is valid and not a mountain, add to to_visit array.
+        iy = i + iy
+        jx = j + jx
         if 0 <= iy < h and 0 <= jx < w and (iy,jx) not in visited and grid[iy][jx] != "^":
-            grid[iy][jx] = grid[current[0]][current[1]] + 1
+            grid[iy][jx] = grid[i][j] + 1
             visited.add((iy,jx))
             to_visit.append((iy,jx))
 
-# Output the distance from Brodo to Mount Boom.
+# Print the length from Brodo's starting position and Mount Boom.
 print(f"{grid[boom[0]][boom[1]]} leagues")
