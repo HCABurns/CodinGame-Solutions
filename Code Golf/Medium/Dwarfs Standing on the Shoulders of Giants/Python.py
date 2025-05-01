90502f9372e51cae290850a2130424b259df07de
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2025-05-01 01:17:07 +0100

    Update Python.py

--- Code Golf/Medium/Dwarfs Standing on the Shoulders of Giants/Python.py
+++ Code Golf/Medium/Dwarfs Standing on the Shoulders of Giants/Python.py
@@ -1,12 +1,11 @@
 h={}
-for i in range(int(input())):
+for i in " "*int(input()):
  x,y=input().split()
  if x not in h:h[x]=[]
  h[x]+=[y]
 def f(n):
- c=h.get(n,[])
- l=1
+ c=h.get(n,[]);l=1
  for i in c:
   if r:=f(i):l=max(l,r+1)
  return l
-print(max([f(i)for i in h]))
+print(max(f(i)for i in h))
