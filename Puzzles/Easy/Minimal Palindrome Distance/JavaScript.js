23cf0a61c1532f7a315bf91216b0988276520be4
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2025-06-06 01:25:21 +0100

    Create JavaScript.js

---
+++ Puzzles/Easy/Minimal Palindrome Distance/JavaScript.js
@@ -0,0 +1,19 @@
+const n = parseInt(readline());
+const s = readline();
+
+// Find Longest palindrome starting from the back.
+var longest = 1;
+for (var j = 0; j < n; ++j) {
+    var isPalindrome = true;
+    for (var k = 0; k <= j; ++k) {
+        if (s[n - j - 1 + k] != s[n - 1 - k]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+    if (isPalindrome && j + 1 > longest) {
+        longest = j + 1;
+    }
+}
+// Print number of chars to be added to the end.
+console.log(n-longest);
