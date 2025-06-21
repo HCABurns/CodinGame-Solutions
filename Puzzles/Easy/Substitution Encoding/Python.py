a601007c336ebc7b63647a60cd72bfb19df3cf35
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2025-06-21 10:51:21 +0100

    Delete Puzzles directory

--- Puzzles/Easy/Substitution Encoding/Python.py
+++
@@ -1,9 +0,0 @@
-# Form hashmap of characters and their positions.
-chars = {}
-rows = int(input())
-for i in range(rows):
-    chars.update({char: [i,j] for j,char in enumerate(input().split(" "))})
-
-# Output the coordinates of the message as a single string.
-for char in input():
-    print(*chars[char],sep="",end="")
