0f296986b87cbe2a89f73534aaeea3ef2211a92c
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2025-06-21 10:36:27 +0100

    Delete Code Golf/Very Hard directory

--- Code Golf/Very Hard/The Resistance/Ruby 299.rb
+++
@@ -1,6 +0,0 @@
-m=gets.chop
-w=[]
-gets.to_i.times{w<<gets.bytes.map{|z|%w[.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..][z-65]}.join}
-c={m.size=>1}
-s=->(i,m,w,c){return c[i]if c[i];t=0;w.each{|x|t+=s[i+x.size,m,w,c]if m[i,x.size]==x};c[i]=t}
-p s[0,m,w,c]
--- Code Golf/Very Hard/The Resistance/Ruby 335.rb
+++
@@ -1,6 +0,0 @@
-m=gets.chop
-w=[]
-gets.to_i.times{w<<gets.chomp.chars.map{|c|Hash[('A'..'Z').zip %w[.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..]][c]}.join}
-c={}
-s=->(i,m,w,c){return 1if i==m.size;return c[i]if c[i];t=0;w.each{|x|t+=s[i+x.size,m,w,c]if m[i,x.size]==x};c[i]=t}
-p s[0,m,w,c]
