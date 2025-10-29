53aacbd0fc6d2b27f3b124f7d6ac14b9634bda66
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2025-10-29 11:17:52 +0000

    Update Solution.py

--- Created Contributions/Snap!/Solution.py
+++ Created Contributions/Snap!/Solution.py
@@ -2,7 +2,6 @@ from collections import deque
 
 p1 = deque([])
 p2 = deque([])
-p1_score = p2_score = 0
 
 m = int(input())
 for i in range(m):
@@ -14,7 +13,6 @@ for i in range(n):
     card = input()
     p2.append(card[0])
 
-
 pile = []
 turn = 1
 
@@ -23,25 +21,25 @@ while p1 and p2:
     if turn == 1:
         pile.append(p1.popleft())
         if len(pile) > 1 and pile[-1] == pile[-2]:
-            p1_score += len(pile)
             while pile:
                 p1.append(pile.pop())
             continue
         turn = 2
+        continue
 
     if turn == 2:
         pile.append(p2.popleft())
         if len(pile) > 1 and pile[-1] == pile[-2]:
-            p2_score += len(pile)
             while pile:
                 p2.append(pile.pop())
             continue
         turn = 1
+        continue
 
-if p1_score > p2_score:
+if p1:
     print("Winner:","Player 1")
-elif p2_score > p1_score:
+elif p2:
     print("Winner:","Player 2")
 else:
     print("Draw")
-print(max(p1_score,p2_score))
+print(max(len(p1),len(p2)))
