cea4b7ac2b1b6fe8168ea3c5ce1504873f1ec895
Harry Burns <78239975+HCABurns@users.noreply.github.com>
2026-01-19 15:33:17 +0000

    Update Solution.py

--- Created Contributions/Snap!/Solution.py
+++ Created Contributions/Snap!/Solution.py
@@ -6,32 +6,44 @@ p2 = deque([])
 m = int(input())
 for i in range(m):
     card = input()
-    p1.append(card[:-1])
+    p1.append([card[:-1],card[-1]])
 n = int(input())
 for i in range(n):
     card = input()
-    p2.append(card[:-1])
+    p2.append([card[:-1],card[-1]])
+
+# Define Suit order
+order = {"S":3,"D":2,"H":1,"C":0}
 
 # Define central pile and starting turn
 pile = []
 turn = 1
 
+"""
+Function to claim the cards in the central pile in case of snap.
+"""
+def snap(card1, card2):
+    if order[card1[1]] > order[card2[1]]:
+        deck = p1
+    else:
+        deck = p2
+    while pile:
+        deck.append(pile.pop())
+
 # Simulate Snap until one player has no more cards.
 while p1 and p2:
     if turn == 1:
         pile.append(p1.popleft())
-        if len(pile) > 1 and pile[-1] == pile[-2]:
-            while pile:
-                p1.append(pile.pop())
+        if len(pile) > 1 and pile[-1][0] == pile[-2][0]:
+            snap(pile[-1], pile[-2])
             continue
         turn = 2
         continue
 
     if turn == 2:
         pile.append(p2.popleft())
-        if len(pile) > 1 and pile[-1] == pile[-2]:
-            while pile:
-                p2.append(pile.pop())
+        if len(pile) > 1 and pile[-1][0] == pile[-2][0]:
+            snap(pile[-1], pile[-2])
             continue
         turn = 1
         continue
